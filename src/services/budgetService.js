78cc5fd876c30c38240884099d9cc0a13b1744b3
kaylaknight385 <kaylaknight385@gmail.com>
2026-02-17 18:16:51 -0500

    budget services done

--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -12,5 +12,16 @@ export const getBudgets = async (id) => {
 };
 
 export const createBudget = async (budgetData) => {
-    const response = await api.post('/budgets')
-}
+    const response = await api.post('/budgets'. budgetData);
+    return response.data;
+};
+
+export const updateBudget = async (id, budgetData) => {
+    const response = await api.put(`/budgets/${id}`, budgetData);
+    return response.data;
+};
+
+export const deleteBudget = async (id) => {
+    const response = await api.delete(`/budgets/${id}`);
+    return response.data;
+};
